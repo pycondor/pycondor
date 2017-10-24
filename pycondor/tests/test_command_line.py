5d4aee4dfa546cf174987d2e19c4d14656926ed7
James Bourbeau <jrbourbeau@gmail.com>
2017-10-23 20:22:57 -0500

    Fixes new flake8 release (v3.5.0) incompatibilities

--- pycondor/tests/test_command_line.py
+++ pycondor/tests/test_command_line.py
@@ -1,7 +1,6 @@
 
 import pytest
 from datetime import datetime
-import subprocess
 from pycondor.command_line import line_to_datetime, progress_bar_str
 
 
@@ -13,19 +12,8 @@ def test_line_to_datetime():
     assert dt == test_datetime
 
 
-def test_progress_bar_str_fail():
+def test_progress_bar_str_type_fail():
     with pytest.raises(TypeError) as excinfo:
         progress_bar_str('not-a-status-object', datetime.now(), datetime.now())
     error = 'status must be of type Status'
     assert error == str(excinfo.value)
-
-
-def test_dagman_progress_fail():
-    bad_file = '/path/to/non-existant/submit_file.submit'
-    cmd = 'dagman_progress {}'.format(bad_file)
-    proc = subprocess.Popen([cmd], stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE, shell=True)
-    out, err = proc.communicate()
-    err_message = 'IOError: Dagman submit file {} doesn\'t exist'.format(
-                                                                    bad_file)
-    assert err_message in err
