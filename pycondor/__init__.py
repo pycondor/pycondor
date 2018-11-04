2b216dfbebd2fa76ec42e279cff0823401bccd67
James Bourbeau <jrbourbeau@gmail.com>
2018-11-04 10:12:59 -0600

    Add visualize

--- pycondor/__init__.py
+++ pycondor/__init__.py
@@ -1,6 +1,7 @@
 
 from .job import Job
 from .dagman import Dagman
+from .visualize import visualize
 from . import utils
 
 from .__version__ import __version__
---
+++ pycondor/tests/test_visualize.py
@@ -0,0 +1,63 @@
+
+import pytest
+graphviz = pytest.importorskip('graphviz')  # noqa: E402
+
+import os
+from pycondor.job import Job
+from pycondor.dagman import Dagman
+from pycondor.visualize import visualize, extract_format, dag_to_graphviz
+from pycondor.utils import clear_pycondor_environment_variables
+
+clear_pycondor_environment_variables()
+
+here = os.path.abspath(os.path.dirname(__file__))
+example_script = os.path.join(here, 'example_script.py')
+
+
+@pytest.fixture()
+def dagman(tmpdir_factory):
+    dag = Dagman(name='example_dagman')
+
+    merge = Job(name='merge',
+                executable='merge.py',
+                dag=dag)
+
+    for i in range(5):
+        processing = Job(name='processing_{}'.format(i),
+                         executable='process.py',
+                         dag=dag)
+        merge.add_parent(processing)
+
+    cleanup_dag = Dagman(name='cleanup',
+                         dag=dag)
+    cleanup_dag.add_parent(merge)
+    return dag
+
+
+def test_visualize_save_file(dagman, tmpdir):
+    filename = str(tmpdir.join('viz.png'))
+    visualize(dagman, filename)
+    assert os.path.exists(filename)
+
+
+@pytest.mark.parametrize('filename, expected', [
+    ('myfile.png', 'png'),
+    ('myfile.pdf', 'pdf'),
+    ('myfile.dot', 'dot'),
+    ('myfile.svg', 'svg'),
+    ('myfile.jpeg', 'jpeg'),
+    ('myfile.jpg', 'jpg'),
+])
+def test_extract_format(filename, expected):
+    assert extract_format(filename) == expected
+
+
+def test_extract_format_invalid_format(dagman):
+    with pytest.raises(ValueError) as excinfo:
+        extract_format('dag_graph.csv')
+    assert 'invalid format' in str(excinfo.value).lower()
+
+
+def test_dag_to_graphviz(dagman):
+    g = dag_to_graphviz(dagman)
+    assert isinstance(g, graphviz.Digraph)
